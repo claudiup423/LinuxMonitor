0dc3c2cfda8130a645fc365800aeda4ab44e8d74
Claudiu <claudiupop182@gmail.com>
2021-06-26 14:50:22 +0100

    First commit

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/format.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/linux_parser.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/main.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/ncurses_display.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/process.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/processor.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/src/system.cpp" "/home/claudiu/portofoliu_ws/src/Application-Development/SystemMonitor/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
